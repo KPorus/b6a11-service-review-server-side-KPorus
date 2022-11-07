cc5f658b722f5b0c808fa4347a06115256270153
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-11-07 22:48:42 +0600

    work data

--- index.js
+++ index.js
@@ -14,20 +14,39 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://Porus:<password>@cluster0.1rqmivg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rqmivg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const workCollection = client.db('photographer').collection('work');
+
+        app.get('/work', async (req, res) => {
+            const query = {}
+            const cursor = workCollection.find(query);
+            const work = await cursor.toArray();
+            res.send(work);
+        });
+
+        app.use((req, res, next) => {
+            console.log(req.path, "I am watching you.")
+            next();
+        })
+
+        app.get('/', (req, res) => {
+            res.send(' server is running')
+        })
 
 
-app.use((req, res, next)=>
-{
-    console.log(req.path,"I am watching you.")
-    next();
-})
+    }
+    finally {
 
-app.get('/', (req, res) => {
-    res.send(' server is running')
-})
+    }
+
+}
+
+run().catch(err => console.error(err));
+
 
 app.listen(port, (req, res) => {
     console.log(` server running on ${port}`);
