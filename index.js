83ad2d021d50eccaf51e9813dfbc7ca83084b7bd
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-11-08 20:56:04 +0600

    updata

--- index.js
+++ index.js
@@ -84,6 +84,15 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         });
+
+        // order delete
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // services
         app.get('/services', async (req, res) => {
             const query = {}
