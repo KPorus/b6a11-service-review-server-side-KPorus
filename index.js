faced4481a1db766c00bae18fb1f5d4cf7a79abc
Fardin Khan_451 <designerfardin2021@gmail.com>
2022-11-09 00:58:49 +0600

    jwt

--- index.js
+++ index.js
@@ -21,12 +21,39 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rqmivg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         const workCollection = client.db('photographer').collection('work');
         const homeCollection = client.db('photographer').collection('home-part-service');
         const serviceCollection = client.db('photographer').collection('services');
         const orderCollection = client.db('photographer').collection('orders');
+        const reviewCollection = client.db('photographer').collection('userReviews');
+
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
+            res.send({token})
+        })  
+
 
         app.get("/",(req,res)=>
         {
@@ -35,14 +62,14 @@ async function run() {
         // work data load
         app.get('/work', async (req, res) => {
             const query = {}
-            const cursor = workCollection.find(query);
+            const cursor =await workCollection.find(query);
             const work = await cursor.toArray();
             res.send(work);
         });
         // home service part
         app.get('/homeService', async (req, res) => {
             const query = {}
-            const cursor = homeCollection.find(query);
+            const cursor = await homeCollection.find(query);
             const home = await cursor.toArray();
             res.send(home);
         })
@@ -67,12 +94,12 @@ async function run() {
         })
 
         // order get 
-        app.get('/orders', async (req, res) => {
-            // const decoded = req.decoded;
+        app.get('/orders', verifyJWT,async (req, res) => {
+            const decoded = req.decoded;
             
-            // if(decoded.email !== req.query.email){
-            //     res.status(403).send({message: 'unauthorized access'})
-            // }
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
 
             let query = {};
             if (req.query.email) {
@@ -86,7 +113,7 @@ async function run() {
         });
 
         // order delete
-        app.delete('/orders/:id', async (req, res) => {
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
@@ -94,7 +121,7 @@ async function run() {
         })
 
         // services
-        app.get('/services', async (req, res) => {
+        app.get('/services', verifyJWT,async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
@@ -112,6 +139,7 @@ async function run() {
             res.send(serviceCheckout);
         })
 
+     
     }
     finally {
 
